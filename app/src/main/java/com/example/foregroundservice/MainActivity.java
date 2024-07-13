713f822b8ad698c150220e40b5b103187eed7cfa
GusJTT <gturpot@unsa.edu.pe>
2024-07-12 19:36:45 -0500

    Añadir interfaz para el cuadro

--- app/src/main/java/com/example/foregroundservice/MainActivity.java
+++ app/src/main/java/com/example/foregroundservice/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btnReproducir = findViewById(R.id.btnReproducir);
+        btnReproducir = findViewById(R.id.btnEscucharDescripcion);
         btnReproducir.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
