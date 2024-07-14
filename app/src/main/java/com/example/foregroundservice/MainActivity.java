bca1ab963774537d8cb98804cea35b323c38db01
GusJTT <gturpot@unsa.edu.pe>
2024-07-14 01:35:20 -0500

    Implementar controles de reproduccion de audio en foregrund y actualizacion de notificaciones

--- app/src/main/java/com/example/foregroundservice/MainActivity.java
+++ app/src/main/java/com/example/foregroundservice/MainActivity.java
@@ -12,6 +12,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     ImageButton btnReproducir;
+    ImageButton btnDetener;
+    ImageButton btnPausar;
+    ImageButton btnReiniciar;
+    String tituloCuadro = "La Otra Ciudad";
     String descripcion = "“La Otra Ciudad” se perfila como una invitación a lo inadvertido. " +
             "La urbe convencional que es desafiada por la mirada atenta del artista, " +
             "revela una belleza latente sumergida entre la velocidad de la cotidianeidad. " +
@@ -22,19 +26,77 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView txtDetObraTitulo = findViewById(R.id.txtDetObraTitulo);
+        txtDetObraTitulo.setText(tituloCuadro);
         TextView txtDetObraDescripcion = findViewById(R.id.txtDetObraDescripcion);
         txtDetObraDescripcion.setText(descripcion);
         btnReproducir = findViewById(R.id.btnReproducir);
+        btnDetener = findViewById(R.id.btnDetener);
+        btnPausar = findViewById(R.id.btnPausar);
+        btnReiniciar = findViewById(R.id.btnReiniciar);
+
         btnReproducir.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startAudioService();
             }
         });
+        btnDetener.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pararReproduccion();
+            }
+        });
+        btnPausar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pausarReproduccion();
+            }
+        });
+        btnReiniciar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                reiniciarReproduccion();
+            }
+        });
+
     }
 
     private void startAudioService() {
         Intent serviceIntent = new Intent(this, ServicioAudio.class);
+        serviceIntent.putExtra("nombreCuadro",tituloCuadro);
+        serviceIntent.putExtra("nombreArchivo","audio_cuadro.mp3");
+        serviceIntent.putExtra("control","reproducir");
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            startForegroundService(serviceIntent);
+        } else {
+            startService(serviceIntent);
+        }
+    }
+    private void pararReproduccion(){
+        Intent serviceIntent = new Intent(this, ServicioAudio.class);
+        serviceIntent.putExtra("nombreCuadro",tituloCuadro);
+        serviceIntent.putExtra("control","parar");
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            startForegroundService(serviceIntent);
+        } else {
+            startService(serviceIntent);
+        }
+    }
+    private void pausarReproduccion(){
+        Intent serviceIntent = new Intent(this, ServicioAudio.class);
+        serviceIntent.putExtra("nombreCuadro",tituloCuadro);
+        serviceIntent.putExtra("control","pausar");
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            startForegroundService(serviceIntent);
+        } else {
+            startService(serviceIntent);
+        }
+    }
+    private void reiniciarReproduccion(){
+        Intent serviceIntent = new Intent(this, ServicioAudio.class);
+        serviceIntent.putExtra("nombreCuadro",tituloCuadro);
+        serviceIntent.putExtra("control","reiniciar");
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             startForegroundService(serviceIntent);
         } else {
--- app/src/main/java/com/example/foregroundservice/ServicioAudio.java
+++ app/src/main/java/com/example/foregroundservice/ServicioAudio.java
@@ -1,39 +1,67 @@
 package com.example.foregroundservice;
 
+import static android.content.ContentValues.TAG;
+
 import android.app.ForegroundServiceStartNotAllowedException;
 import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.Service;
 import android.content.Intent;
 import android.content.pm.ServiceInfo;
+import android.content.res.AssetFileDescriptor;
+import android.media.MediaPlayer;
 import android.os.Build;
 import android.os.IBinder;
+import android.util.Log;
 import android.widget.Toast;
 
-import androidx.annotation.Nullable;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.ServiceCompat;
-import androidx.core.content.ContextCompat;
+
+import java.io.IOException;
 
 public class ServicioAudio extends Service {
-    private String CHANNEL_ID = "canal 1";
+    private String CHANNEL_ID = "canal 100";
+    private MediaPlayer reproductor;
+    private String tituloCuadro = "";
     @Override
     public void onCreate() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            CharSequence name = "Audio playback";
-            String description = "Channel for Audio playback";
-            int importance = android.app.NotificationManager.IMPORTANCE_DEFAULT;
-            android.app.NotificationChannel channel = new android.app.NotificationChannel(CHANNEL_ID, name, importance);
-            channel.setDescription(description);
+            CharSequence nombre = "Reproducción de audio";
+            String descripcion = "Canal para reproducción de audio";
+            int importancia = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel canal = new NotificationChannel(CHANNEL_ID, nombre, importancia);
+            canal.setDescription(descripcion);
             // Registra el canal con el sistema
-            android.app.NotificationManager notificationManager = getSystemService(android.app.NotificationManager.class);
-            notificationManager.createNotificationChannel(channel);
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(canal);
         }
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        Toast.makeText(this, "service starting", Toast.LENGTH_SHORT).show();
-        startForeground();
+        String textoNotificacion = "Reproduciendo audio";
+        tituloCuadro = intent.getStringExtra("nombreCuadro");
+        String nombreArchivo = intent.getStringExtra("nombreArchivo");
+        String control = intent.getStringExtra("control");
+        Log.d(TAG,control);
+        if(control.equals("reproducir")){
+            try {
+                Log.d(TAG,"Inicio Reproduciendo audio");
+                reproducirAudio(nombreArchivo);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        } else if (control.equals("pausar")) {
+            textoNotificacion = "Audio pausado";
+            pausarAudio();
+        } else if (control.equals("reiniciar")){
+            reiniciarAudio();
+        } else if (control.equals("parar")) {
+            detenerAudio();
+        }
+        startForeground(textoNotificacion);
         return START_STICKY;
     }
 
@@ -48,37 +76,73 @@ public class ServicioAudio extends Service {
         Toast.makeText(this, "service done", Toast.LENGTH_SHORT).show();
     }
 
-    private void startForeground(){
+    private void startForeground(String texto){
         try {
             Notification notification =
                     new NotificationCompat.Builder(this, CHANNEL_ID)
-                            .setContentTitle("Servicio de Audio")
-                            .setContentText("Reproduciendo audio...")
+                            .setContentTitle(tituloCuadro)
+                            .setContentText(texto)
                             .setSmallIcon(R.drawable.baseline_audiotrack_24)
                             .build();
             int type = 0;
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
                 type = ServiceInfo.FOREGROUND_SERVICE_TYPE_MEDIA_PLAYBACK;
             }
-            ServiceCompat.startForeground(
-                    /* service = */ this,
-                    /* id = */ 100, // Cannot be 0
-                    /* notification = */ notification,
-                    /* foregroundServiceType = */ type
-            );
+            ServiceCompat.startForeground(this, 100, notification, type);
         } catch (Exception e) {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S &&
                     e instanceof ForegroundServiceStartNotAllowedException
             ) {
-                // App not in a valid state to start foreground service
-                // (e.g started from bg)
+                Toast.makeText(this, "No se puede iniciar el servicio", Toast.LENGTH_SHORT).show();
             }
         }
     }
 
-    private void comandosAudio(){
-        
+    private void reproducirAudio(String archivo) throws IOException {
+        if (archivo != null) {
+            if (reproductor == null) {
+                Log.d(TAG, "Reproduciendo audio");
+                AssetFileDescriptor audioAsset = getAssets().openFd(archivo);
+                reproductor = new MediaPlayer();
+                reproductor.setDataSource(
+                        audioAsset.getFileDescriptor(),
+                        audioAsset.getStartOffset(),
+                        audioAsset.getLength()
+                );
+                audioAsset.close();
+                reproductor.prepare();
+                reproductor.setVolume(1f, 1f);
+                reproductor.setLooping(false);
+                reproductor.start();
+                Toast.makeText(this, "Reproduciendo", Toast.LENGTH_SHORT).show();
+            } else if (!reproductor.isPlaying()) {
+                reproductor.start();
+                Toast.makeText(this, "Reproduciendo", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 
-
+    private void detenerAudio() {
+        if (reproductor != null) {
+            reproductor.stop();
+            reproductor.release();
+            reproductor = null;
+        }
+        Toast.makeText(this, "Detener", Toast.LENGTH_SHORT).show();
+        stopForeground(true);
+        stopSelf();
+    }
+    private void pausarAudio() {
+        if (reproductor != null && reproductor.isPlaying()) {
+            reproductor.pause();
+            Toast.makeText(this, "Pausar", Toast.LENGTH_SHORT).show();
+        }
+    }
+    private void reiniciarAudio() {
+        if (reproductor != null) {
+            reproductor.seekTo(0);
+            reproductor.start();
+            Toast.makeText(this, "Reiniciar", Toast.LENGTH_SHORT).show();
+        }
+    }
 }
